79b5c643bc618a88a456938acaa24e5f2baa226e
bcatek <kiranbcatek@gmail.com>
2025-08-19 17:00:44 +0530

    Add files via upload

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+ruby "3.2.4"   # or the version you have installed
+gem "sinatra"
+gem "puma"
--- app.rb
+++ app.rb
@@ -1,1 +1,20 @@
+require 'sinatra/base'
+require_relative 'routes/login_routes'
+require_relative 'routes/telecall'
+require_relative 'routes/token'
 
+class MainApp < Sinatra:: Base
+    use LoginRoutes
+    use TeleCall
+    use Token
+
+    set :bind, 'localhost'
+    set :port, 4567
+
+    set :public_folder, File.dirname(__FILE__)
+    get '/' do
+        send_file File.join(settings.public_folder, 'index.html')
+    end
+
+    run! if app_file == $0
+end
