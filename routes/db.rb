d3e7dfca1974524875b5ff0a8fc35430105d5124
bcatek <kiranbcatek@gmail.com>
2025-08-19 16:57:09 +0530

    Add files via upload

--- routes/db.rb
+++ routes/db.rb
@@ -1,1 +1,11 @@
+require 'tiny_tds'
 
+def db_client
+TinyTds::Client.new(
+  username: 'propkey2025_SQLLogin_1',   
+  password: 'bfjy4ef6u5',     
+  host: 'getitdb.mssql.somee.com',             
+  port: 1433,                    
+  database: 'getitdb'              
+)
+end
